208b2865536325219a62ec5457675b4ee37502d6
Beyler55 <baylarss@code.edu.az>
2022-07-14 11:02:21 +0400

    first

---
+++ LeetCode/LeetCode-MoveZeroes/LeetCode-MoveZeroes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace LeetCode_MoveZeroes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 0, 1, 0, 3, 12 };
+            Solution.MoveZeroes(nums);
+        }
+    }
+    public class Solution
+    {
+        public static void MoveZeroes(int[] nums)
+        {
+            
+            int i = 0, j = 0;
+            while (j < nums.Length)
+            {
+                if (nums[j] == 0)
+                {
+                    j++;
+                }
+                else
+                {
+                    int temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                    i++;
+                    j++;
+                }
+            }
+            foreach (var item in nums)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ LeetCode/LeetCode-SingleNumber/LeetCode-SingleNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode_SingleNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1,1,1,5,4,4};
+            Solution sl = new Solution();
+            Console.WriteLine(  sl.SingleNumber(num));
+        }
+    }
+    public class Solution
+    {
+       
+        public int SingleNumber(int[] nums)
+        {
+            int res = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+                res = res ^ nums[i];
+            return res;
+        }
+       
+    }
+}
